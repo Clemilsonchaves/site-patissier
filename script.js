8f6ab485026fffb252a93b476fa65105065d5076
Clemilson Chaves <108312437+Clemilsonchaves@users.noreply.github.com>
2023-04-25 09:15:32 -0300

    Refatoração dos Códigos

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
 })
+
+
+let count = 1;
+
+document.getElementById("radio1").checkd = true;
+
+setInterval(function () {
+    nextImage();
+}, 6000)
+
+function nextImage() {
+    count++; 
+    if (count >3){
+        count = 1;
+    }
+
+    document.getElementById("radio" +count).checked = true;
+}
